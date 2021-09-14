28ed9e7b2dbe931ab873ed01a57bfc8719a445a6
Yashashav <krishnasaxena271@gmail.com>
2021-09-14 18:20:20 +0530

    Adding Script For Search Button

---
+++ search.js
@@ -0,0 +1,19 @@
+function searchbtn() {
+    // Declare variables
+    var input, filter, ul, li, button, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      button = li[i].getElementsByTagName("button")[0];
+      txtValue = button.textContent || button.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
